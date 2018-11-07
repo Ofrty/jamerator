588b47a74598cde03a77f63a3f2281c856dc7554
Ofrty <24553692+Ofrty@users.noreply.github.com>
2018-11-06 20:06:19 -0500

    scale repeat fix; rudimentary styling
    -scales no longer duplicate
    -rudimentary styling added; Obsidian theme used

--- jamerator.js
+++ jamerator.js
@@ -13,7 +13,6 @@
 /*getRandomIdx() returns a randomly-selected index in an array passed in ar
 * Assumption: arg1 'arr' is an array. Returns nothing otherwise.
 * */
-
 function getRandomIdx(arr)
 {
     return (Math.floor((Math.random() * ((arr.length - 1) + 1))));
@@ -22,7 +21,6 @@ function getRandomIdx(arr)
 /*getRandomVal() returns the contents of a randomly-selected index in the array passed in arg
 * Assumption: arg1 'arr' is an array. Just returns nothing otherwise.
 * */
-
 function getRandomVal(arr)
 {
     return arr[getRandomIdx(arr)];
@@ -31,7 +29,6 @@ function getRandomVal(arr)
 /*writeScaleTab() sets the contents of a scale tab block to contain the elements of the scale it is passed.
 arg1=scale order n, arg2=scale object
  */
-
 function writeScaleTab(ordinal, scale)
 {
     //set the scale header text to the scale name
@@ -54,7 +51,6 @@ function writeScaleTab(ordinal, scale)
 /*setScaleHeader() sets the header elem for a scale to the name of the scale.
 * arg1=scale n, arg2=scale name
  */
-
 function setScaleHeader(ordinal, name)
 {
     //find the appropriate element by id
@@ -67,7 +63,6 @@ function setScaleHeader(ordinal, name)
 
 /*setFret() sets the contents of a fret div element to the value in val.
 * arg1=scale order n, arg2=string n, arg3=note n, arg4=val to be inserted*/
-
 function setFret(ordinal, s, n, val)
 {
     //find the appropriate elem by id
@@ -150,6 +145,13 @@ var tempoOps =
         , "140"
     ];
 
+/*each scale is an object consisting of 3 parts:
+ 1) a name
+ 2) a 2D-array of frets ordered strings 1-6
+ 3) an index of the root note. use the note ref here:
+        A   A#/Bb   B   C   C#/Db   D   D#/Eb   E   F   F#/Gb   G   G#/Ab
+        1     2     3   4     5     6     7     8   9    10    11    12
+ */
 var scaleOps =
     [
         {name: "Root 6 Index/3 Per String", frets: [[2, 4, 5], [2, 4, 5], [1, 2, 4], [1, 2, 4], [0, 2, 4], [0, 2, 4]], root: 8}
@@ -166,10 +168,6 @@ var scaleOps =
         //, {name:  "Root 4 Diminished (child variants)", frets: [[],[],[],[],[],[]]}
     ];
 
-/* note ref
-A A#/Bb B C C#/Db D D#/Eb E F F#/Gb G G#/Ab
-1   2   3 4   5   6   7   8 9  10  11  12
-*/
 //TODO: finish tabbing out martino stuff
 //TODO: add a +12 bool for each scale; or maybe, just disregard this and make it an assumption of its usage
 
@@ -181,14 +179,41 @@ var prog = getRandomVal(progOps);
 var style = getRandomVal(styleOps);
 var feel = getRandomVal(feelOps);
 var tempo = getRandomVal(tempoOps);
-var youTube = "https://www.youtube.com/results?search_query=" + rootChord + '+' + prog + '+' + style + '+' + "backing track";
+var youTube = "https://www.youtube.com/results?search_query=" + rootChord + '+' + prog + '+' + style + '+' + "backing";
 
 //scales to use are generated a bit more complexly.
 var scales = [];
+var tempScaleOp;
+var isRepeat;
+var scaleCount = 3;
+var c = 0;
 
-scales.push(getRandomVal(scaleOps));
-scales.push(getRandomVal(scaleOps));
-scales.push(getRandomVal(scaleOps));
+//create as many scales as specified. ensure no duplicates
+while (c < scaleCount)
+{
+    isRepeat = false;
+
+    //get a random scale
+    tempScaleOp = getRandomVal(scaleOps);
+
+    //ensure that it hasn't already been selected
+    for (var j = 0; (j < scales.length) && !(isRepeat); j++)
+    {
+        //alert(tempScaleOp.name + " vs " + scales[j].name); //debug
+
+        if ((tempScaleOp.name) === scales[j.name])
+        {
+            isRepeat = true;
+        }
+    }
+
+    //if not a repeat, go ahead and add to the scales list. else do nada and restart, picking again.
+    if (!isRepeat)
+    {
+        scales.push(tempScaleOp);
+        c++;
+    }
+}
 
 /**populate elems**/
 elRootChord.textContent += rootChord;
